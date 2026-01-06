62d6ff2ea2ec74dcd704eabc3bf26e87b36d6046
MdMamunOrRashid <mdmamunhossen786@gmail.com>
2026-01-06 22:13:05 +0600

    some error on random color

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ const body=document.querySelector("body")
 function setColor(paramiter){
     body.style.background=paramiter;
 }
+function randomColor(){
+
+    const red= Math.random(Math.round()*255)
+    const green= Math.random(Math.round()*255)
+    const blue= Math.random(Math.round()*255)
+
+    const color= `rgb(${red}, ${green}, ${blue})`
+
+    body.style.background=color;
+}
